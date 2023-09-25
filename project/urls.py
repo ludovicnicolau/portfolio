5a09346b0699839b56bcb9c4bde3ab822f61e196
= <ludovic.nicolau@gmail.com>
2023-09-25 21:39:18 +0200

    404.html now has working css

--- project/urls.py
+++ project/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cv/', include("cv.urls")),
+    path('', TemplateView.as_view(template_name='404.html'))
 ]
