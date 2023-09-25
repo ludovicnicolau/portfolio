9d2aac56f294f117d51c9a361905b8ae1fdfce53
= <ludovic.nicolau@gmail.com>
2023-09-25 21:40:33 +0200

    Removed debug pages

--- project/urls.py
+++ project/urls.py
@@ -17,10 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.views.generic import TemplateView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cv/', include("cv.urls")),
-    path('', TemplateView.as_view(template_name='404.html'))
 ]
